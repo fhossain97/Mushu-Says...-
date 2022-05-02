8aa7d3f2c817ee42400ccf664fab7b2b760458dd
fhossain <farhana.hossain@aol.com>
2022-05-02 00:03:08 -0400

    successfully added player name input

--- app.js
+++ app.js
@@ -1,74 +1,24 @@
-let indigo = document.querySelector('.color-indigo')
-let orange = document.querySelector('.color-orange')
-let greenyellow = document.querySelector('.color-reenyellow')
-let red = document.querySelector('.color-red')
-let yellow = document.querySelector('.color-yellow')
-let blue = document.querySelector('.color-blue')
-let pink = document.querySelector('.color-pink')
-let green = document.querySelector('.color-green')     
-let violet = document.querySelector('.color-violet') 
-
-
-//Enter your name
+//Enter your name to play the game
 let playerName = () => {
-    let player = document.querySelector('#enteredname')
-    let nameEntered = []
+    let player = prompt('Enter your name!', 'Name')
 
-    if (player.value !== '') {
-        nameEntered.push(player.value)
-        return(nameEntered)
+    if (player != null) {
+        document.querySelector('.player-name').innerHTML = 'Player: ' + player
     }
 }
+playerName()
 
-//Start
-let start = () => {
-    let gameStartButton = document.querySelector('#start')
-startGame = () => {
-gameStartButton.style.display = 'none';
-gameRounds()
-}
-gameStartButton.addEventListener('click', startGame)
-}
-start();
-
-//Rounds
-let round = 0;
-let gameRounds = () => {
-    round =+ 1
-    nextPattern()
-}
-
-//Next Pattern
-let nextPattern = () => {
-    const squareSequence = []
-    squareSequence.push(randomSquares())
-}
-
-//Random Square Pattern
-let randomSquares = () => {
-    const squares = [indigo, orange, greenyellow, red, yellow, blue, pink, green, violet];
-    const random = squares[Math.floor(Math.random() * squares.length)] 
-    return random;   
-}
-
-//Flashing Squares
-let flashingSquares = (colorChange) => {
-    let square = document.querySelector('.square')
-        square.classList.add('color-change')
-        setTimeout(() => {
-            square.classList.remove('color-change')
-        }, 250)
-    }
-
-    flashingSquares()
-
-
-
-
-
-
-
-
-
-
+//Start button to start the game
+//Count each round of the game
+//Reset function within each level
+//Each round will signal the sequence to begin
+//Each sequence must be random
+//Each random sequence will flash a random pattern 
+//Each random pattern will flash a square pattern
+//Each pattern must be looped
+//As you click each square, you gain one point for each correct click
+//As you proceed to each level, there will be an animation pop up in between
+//Up to level 10
+//Winner function
+//Ability to restart the game
 
--- mock.js
+++ mock.js
@@ -63,3 +63,109 @@
 
 
 
+let indigo = document.querySelector('#indigo')
+let orange = document.querySelector('#orange')
+let greenyellow = document.querySelector('#greenyellow')
+let red = document.querySelector('#red')
+let yellow = document.querySelector('#yellow')
+let blue = document.querySelector('#blue')
+let pink = document.querySelector('#pink')
+let green = document.querySelector('#green')     
+let violet = document.querySelector('#violet') 
+
+
+//Enter your name
+let playerName = () => {
+    let player = document.querySelector('#enteredname')
+    let nameEntered = []
+
+    if (player.value !== '') {
+        nameEntered.push(player.value)
+        return(nameEntered)
+    }
+}
+
+//Start
+let start = () => {
+    let gameStartButton = document.querySelector('#start')
+startGame = () => {
+gameStartButton.style.display = 'none';
+gameRounds()
+}
+gameStartButton.addEventListener('click', startGame)
+}
+start();
+
+//Rounds
+let round = 0;
+let gameRounds = () => {
+    round =+ 1
+    nextPattern()
+}
+
+//Next Pattern
+let nextPattern = () => {
+    const squareSequence = []
+    squareSequence.push(randomSquares())
+}
+
+//Random Square Pattern
+let randomSquares = () => {
+    const squares = [indigo, orange, greenyellow, red, yellow, blue, pink, green, violet];
+    const random = squares[Math.floor(Math.random() * squares.length)] 
+    return random;   
+}
+
+//Flashing Squares
+let flashingSquares = (colorChange) => {
+    let square = document.querySelector('.square')
+        square.classList.add('color-change')
+        setTimeout(() => {
+            square.classList.remove('color-change')
+        }, 250)
+    }
+
+
+    //Pattern looped
+    let patternLoop = (nextPattern) => {
+        nextPattern.forEach(() => {
+            setTimeout(() => {
+                flashingSquares(colorChange)
+            })
+        })
+    }
+
+    //Score
+let score = () => {
+    if (squareClicked === pattern) {
+        score += 1
+    } else if (squareClicked !== pattern) {
+        alert('Wrong square has been clicked. Reset the level.')
+    }
+
+}
+
+
+
+//Winner
+// function displayWinner(winner){ 
+//     const winnerText = document.createElement('body');
+//     winnerText.innerText = winner;
+//     document.body.appendChild(winnerText);
+//   }
+//   compareChoices();   
+  
+//   document.querySelectorAll('button').onclick = function player() {
+//    console.log(gameChoice. target);
+//   }
+
+
+
+
+
+
+
+
+
+
+
